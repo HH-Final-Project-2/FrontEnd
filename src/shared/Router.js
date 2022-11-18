93049a87b8bc1f4b29cef92b005e921a6a32646e
ssssssg-hub <83156255+ssssssg-hub@users.noreply.github.com>
2022-11-19 00:20:46 +0900

    깃허브 에러 테스트

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,10 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CompanySearch from "../components/main/CompanySearch";
-import MainCards from "../components/main/MainCards";
-import MainCardsPut from "../components/main/MainCardsPut";
-import OtherCardsCategory from "../components/main/OtherCardsCategory";
-import ViewMainDetailPost from "../components/main/ViewMainDetailPost";
 import Main from "../pages/Main";
 
 const Router = () => {
@@ -12,11 +7,6 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/otherCategory" element={<OtherCardsCategory />} />
-        <Route path="/posts" element={<MainCards />} />
-        <Route path="/posts/:id/put" element={<MainCardsPut />} />
-        <Route path="/posts/companySearch" element={<CompanySearch />} />
-        <Route path="/posts/get/:id" element={<ViewMainDetailPost />} />
       </Routes>
     </BrowserRouter>
   );
