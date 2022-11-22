9b43dcec74235d1c0e02d7e4e7b53cd6d5243979
andamiro98 <rlatmdwo9@gmail.com>
2022-11-23 05:39:04 +0900

    [주소에 따른 지도 위도,경도 불러오기 완료] [주소 검색후 input value 할당 완료]

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -17,6 +17,7 @@ import MyCard from "../pages/MyCard";
 import MyCardInfo from "../components/myCard/MyCardInfo";
 import MyCardPatch from "../components/myCard/MyCardPatch";
 import MyCardMake from "../components/myCard/MyCardMake";
+import MyCardCompanySerach from "../components/myCard/MyCardCompanySerach";
 import Chat from "../pages/Chat";
 import Chatroom from "../components/chat/Chatroom";
 
@@ -48,6 +49,7 @@ const Router = () => {
         <Route path="/mypage/cardinfo" element={<MyCardInfo />} />
         <Route path="/mypage/cardmake" element={<MyCardMake />} />
         <Route path="/mypage/cardpatch" element={<MyCardPatch />} />
+        <Route path="/mypage/cardpatch/MyCardCompanySerach" element={<MyCardCompanySerach />} />
 
         {/* 채팅 */}
         <Route path="/chat" element={<Chat />} />
