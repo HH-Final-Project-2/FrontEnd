9423afca30583fd496e7f87cf3c2831800868feb
andamiro98 <rlatmdwo9@gmail.com>
2022-11-24 04:26:31 +0900

    [바텀시트 수정, 명함 make 회사 검색]

--- src/components/myCard/MyCardMake.jsx
+++ src/components/myCard/MyCardMake.jsx
@@ -18,7 +18,7 @@ const MyCardMake = () => {
     email: '',
     phoneNum: '',
     company: '',
-    // companyAddress:'',
+    companyAddress: '',
     department: '',
     position: '',
     tel: '',
@@ -31,6 +31,7 @@ const MyCardMake = () => {
     email,
     phoneNum,
     company,
+    companyAddress,
     department,
     position,
     tel,
@@ -111,6 +112,31 @@ const MyCardMake = () => {
             }
             onChange={onChage}
           ></St_Address>
+
+          <AddressBox>
+            <div>
+              <svg
+                width="12"
+                height="15"
+                viewBox="0 0 12 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ marginRight: '8px' }}
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M5.66545 14.8907L5.71636 14.9204L5.73673 14.9322C5.81748 14.9767 5.90784 15 5.99964 15C6.09144 15 6.18179 14.9767 6.26255 14.9322L6.28291 14.9211L6.33455 14.8907C6.61899 14.7189 6.89655 14.5356 7.16655 14.3411C7.86551 13.8384 8.51857 13.2726 9.11782 12.6506C10.5316 11.1764 12 8.96141 12 6.11157C12 4.49068 11.3679 2.93618 10.2426 1.79004C9.11742 0.643896 7.5913 0 6 0C4.4087 0 2.88258 0.643896 1.75736 1.79004C0.632141 2.93618 0 4.49068 0 6.11157C0 8.96067 1.46909 11.1764 2.88218 12.6506C3.4812 13.2726 4.13402 13.8384 4.83273 14.3411C5.10296 14.5356 5.38076 14.719 5.66545 14.8907ZM6 8.33396C6.57865 8.33396 7.13361 8.09981 7.54278 7.68304C7.95195 7.26626 8.18182 6.70098 8.18182 6.11157C8.18182 5.52216 7.95195 4.95688 7.54278 4.5401C7.13361 4.12332 6.57865 3.88918 6 3.88918C5.42135 3.88918 4.86639 4.12332 4.45722 4.5401C4.04805 4.95688 3.81818 5.52216 3.81818 6.11157C3.81818 6.70098 4.04805 7.26626 4.45722 7.68304C4.86639 8.09981 5.42135 8.33396 6 8.33396Z"
+                  fill="#BCC2CC"
+                />
+              </svg>
+            </div>
+            <SearchAddress>
+              {searchinfo.companyAddress
+                ? searchinfo.companyAddress
+                : companyAddress}
+            </SearchAddress>
+          </AddressBox>
         </Item>
         <Item>
           <St_Key>직책</St_Key>
@@ -198,4 +224,15 @@ const Item = styled.div`
   margin-top: 10px;
   font-size: 12px;
   color: #6b6b6b;
+`;
+
+const AddressBox = styled.div`
+  margin-left: 8px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const SearchAddress = styled.div`
+  display: flex;
+  width: 250px;
 `;
