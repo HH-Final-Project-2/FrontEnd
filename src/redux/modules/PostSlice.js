f49538b13c20dafecba3a91bc6584a15f64a2098
formaro <formaro99@gmail.com>
2022-11-25 04:56:40 +0900

    게시글 좋아요 추가...!

--- src/redux/modules/PostSlice.js
+++ src/redux/modules/PostSlice.js
@@ -19,11 +19,24 @@ export const __searchPost = createAsyncThunk(
   }
 );
 
+// 게시글 좋아요
+export const __likePost = createAsyncThunk(
+  'post/likePost',
+  async (payload, thunkAPI) => {
+    // console.log(payload)
+    try {
+      const { data } = await instance.post(`/api/auth/post/heart/${payload}`);
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 // 게시글 전체 조회
 export const __getPostAll = createAsyncThunk(
   'posts/getPostAll',
   async (payload, thunkAPI) => {
-    console.log(payload);
     try {
       const { data } = await instance.get('/api/posting');
       return thunkAPI.fulfillWithValue(data.data);
@@ -38,7 +51,12 @@ export const __getPost = createAsyncThunk(
   'post/getPost',
   async (payload, thunkAPI) => {
     try {
+      await instance.post(`/api/auth/post/heart/${payload}`);
+      let likeStore = await instance.post(`/api/auth/post/heart/${payload}`);
+      // console.log('나는 라이크 데이터', likeData.data)
+
       const { data } = await instance.get(`/api/posting/${payload}`);
+      data.data.like = likeStore.data.data;
       return thunkAPI.fulfillWithValue(data.data);
     } catch (error) {
       console.log(error);
@@ -98,7 +116,6 @@ export const __deletePost = createAsyncThunk(
   async (payload, thunkAPI) => {
     try {
       const { data } = await instance.delete(`/api/posting/${payload}`);
-      console.log(data);
       return thunkAPI.fulfillWithValue(payload);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -135,6 +152,12 @@ const initialState = {
     image: '',
     createdAt: '',
     modifiedAt: '',
+    like: false,
+  },
+  like: {
+    success: true,
+    data: false,
+    error: null,
   },
 
   isLoading: false,
@@ -151,6 +174,12 @@ export const PostSlice = createSlice({
     [__searchPost.fulfilled]: (state, action) => {
       state.isLoading = false;
       state.post = action.payload;
+    },
+
+    //게시글 좋아요
+    [__likePost.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.like = action.payload;
     },
 
     //게시글 전체 조회
