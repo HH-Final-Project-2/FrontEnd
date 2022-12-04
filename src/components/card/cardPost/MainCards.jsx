f3da442330a283cf4f122ecdffe971e5bd1a7a0d
ssssssg-hub <83156255+ssssssg-hub@users.noreply.github.com>
2022-12-05 06:10:07 +0900

    회원가입 이메일 인증구현, 명함첩 회사등록 뷰 새로 구현

--- src/components/card/cardPost/MainCards.jsx
+++ src/components/card/cardPost/MainCards.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react';
-import { useNavigate } from 'react-router';
-import Layout from '../../layout/Layout';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import Layout from "../../layout/Layout";
 import {
   __writePost,
   __imgPost,
   __cardInfo,
-} from '../../../redux/modules/CardsSlice';
-import { Link } from 'react-router-dom';
+} from "../../../redux/modules/CardsSlice";
+import { Link } from "react-router-dom";
 import {
   St_Header,
   PatchBox,
@@ -32,7 +32,7 @@ import {
   AddressSearch,
   SectionLine,
   AssistiveText,
-} from './cardPostStyle';
+} from "./cardPostStyle";
 
 const MainCards = () => {
   const navigate = useNavigate();
@@ -44,16 +44,16 @@ const MainCards = () => {
   //state생성
 
   const [cardName, setCardName] = useState(
-    companyGet.cardName ? companyGet.cardName : ''
+    companyGet.cardName ? companyGet.cardName : ""
   );
   const [companyType, setCompanyType] = useState(
-    companyGet.companyType ? companyGet.companyType : ''
+    companyGet.companyType ? companyGet.companyType : ""
   );
   const [department, setDepartment] = useState(
-    companyGet.department ? companyGet.department : ''
+    companyGet.department ? companyGet.department : ""
   );
   const [position, setPosition] = useState(
-    companyGet.position ? companyGet.position : ''
+    companyGet.position ? companyGet.position : ""
   );
   const [email, setEmail] = useState(
     imgGet.email ? imgGet.email : companyGet.email
@@ -70,6 +70,7 @@ const MainCards = () => {
     companyGet.companyAddress ? companyGet.companyAddress : null
   );
   const [showPopup, setShowPopup] = useState(false);
+  const [companyHow, setCompanyHow] = useState("");
   console.log(
     cardName,
     companyType,
@@ -110,7 +111,7 @@ const MainCards = () => {
 
   const isValidEmail =
     email !== undefined && email !== null
-      ? email.includes('@') && email.includes('.')
+      ? email.includes("@") && email.includes(".")
       : false;
 
   const isValidInput =
@@ -137,14 +138,14 @@ const MainCards = () => {
   const mediaChangeHandler = (e) => {
     e.preventDefault();
     const file = new FormData();
-    file.append('cardImg', e.target.files[0]);
+    file.append("cardImg", e.target.files[0]);
     dispatch(__imgPost(file));
   };
 
-  const togglePopup = (event) => {
-    setShowPopup(event.target.value);
-  };
-  //
+  // const togglePopup = (event) => {
+  //   setShowPopup(event.target.value);
+  // };
+  // //
 
   //작성버튼 클릭시 발동되는 함수
   const cardsSubmitHandler = (e) => {
@@ -165,11 +166,11 @@ const MainCards = () => {
         })
       );
 
-      alert('명함 작성 완료!');
-      companyType === 'own' ? navigate('/cards') : navigate('/otherCategory');
+      alert("명함 작성 완료!");
+      companyType === "own" ? navigate("/cards") : navigate("/otherCategory");
       window.location.reload();
     } else {
-      alert('입력한 내용을 확인해주세요');
+      alert("입력한 내용을 확인해주세요");
     }
   };
   //
@@ -182,7 +183,7 @@ const MainCards = () => {
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          style={{ cursor: 'pointer' }}
+          style={{ cursor: "pointer" }}
           onClick={() => {
             // window.location.reload();
             navigate(-1);
@@ -208,8 +209,8 @@ const MainCards = () => {
                 type="radio"
                 id="own"
                 name="companyType"
-                value={'own'}
-                checked={companyType === 'own'}
+                value={"own"}
+                checked={companyType === "own"}
                 onChange={(e) => {
                   setCompanyType(e.target.value);
                 }}
@@ -221,8 +222,8 @@ const MainCards = () => {
                 type="radio"
                 id="other"
                 name="companyType"
-                value={'other'}
-                checked={companyType === 'other'}
+                value={"other"}
+                checked={companyType === "other"}
                 onChange={(e) => {
                   setCompanyType(e.target.value);
                 }}
@@ -279,7 +280,7 @@ const MainCards = () => {
                   src={imgGet.imgUrl}
                   alt="preview-img"
                   id="card"
-                  style={{ margin: 'auto' }}
+                  style={{ margin: "auto" }}
                 />
               )
             )}
@@ -300,7 +301,7 @@ const MainCards = () => {
             type="text"
             placeholder="이름"
             name="cardName"
-            value={cardName || ''}
+            value={cardName || ""}
             minLength="1"
             maxLength="6"
             onChange={(e) => {
@@ -317,14 +318,14 @@ const MainCards = () => {
             type="text"
             placeholder="Ex) 010-0000-0000"
             name="phoneNum"
-            value={phoneNum || ''}
+            value={phoneNum || ""}
             maxLength="13"
             onChange={(e) => {
               setPhoneNum(e.target.value);
             }}
           />
 
-          {phoneNum && phoneNum.includes('-') === false ? (
+          {phoneNum && phoneNum.includes("-") === false ? (
             <AssistiveText>- 을 포함해주세요</AssistiveText>
           ) : null}
         </Item>
@@ -337,7 +338,7 @@ const MainCards = () => {
             type="text"
             placeholder="Ex) abc@gmail.com"
             name="email"
-            value={email || ''}
+            value={email || ""}
             minLength="10"
             maxLength="30"
             onChange={(e) => {
@@ -348,78 +349,41 @@ const MainCards = () => {
             <AssistiveText>이메일을 확인하세요</AssistiveText>
           ) : null}
         </Item>
-
         <SectionLine />
 
         <Item>
           <St_Key>
             회사<Essential>*</Essential>
           </St_Key>
-          <div>
-            <St_value
-              readOnly
-              type="text"
-              name="company"
-              placeholder="회사 검색"
-              readOnly
-              value={company || ''}
-              onChange={(e) => {
-                setCompany(e.target.value);
-              }}
-              onClick={() => {
-                dispatch(
-                  __cardInfo({
-                    cardName: cardName ? cardName : '',
-                    email: email,
-                    phoneNum: phoneNum,
-                    department: department ? department : '',
-                    position: position ? position : '',
-                    tel: tel,
-                    fax: fax,
-                    companyType: companyType ? companyType : '',
-                  })
-                );
-                navigate('/posts/companySearch');
-              }}
-            ></St_value>
-          </div>
-          <St_Address
-            name="companyAddress"
-            value={companyAddress || ''}
-            onChange={(e) => {
-              setCompanyAddress(e.target.value);
-            }}
-          >
-            <AddressBox>
-              <div>
-                <svg
-                  width="12"
-                  height="15"
-                  viewBox="0 0 12 15"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  style={{ marginRight: '8px' }}
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M5.66545 14.8907L5.71636 14.9204L5.73673 14.9322C5.81748 14.9767 5.90784 15 5.99964 15C6.09144 15 6.18179 14.9767 6.26255 14.9322L6.28291 14.9211L6.33455 14.8907C6.61899 14.7189 6.89655 14.5356 7.16655 14.3411C7.86551 13.8384 8.51857 13.2726 9.11782 12.6506C10.5316 11.1764 12 8.96141 12 6.11157C12 4.49068 11.3679 2.93618 10.2426 1.79004C9.11742 0.643896 7.5913 0 6 0C4.4087 0 2.88258 0.643896 1.75736 1.79004C0.632141 2.93618 0 4.49068 0 6.11157C0 8.96067 1.46909 11.1764 2.88218 12.6506C3.4812 13.2726 4.13402 13.8384 4.83273 14.3411C5.10296 14.5356 5.38076 14.719 5.66545 14.8907ZM6 8.33396C6.57865 8.33396 7.13361 8.09981 7.54278 7.68304C7.95195 7.26626 8.18182 6.70098 8.18182 6.11157C8.18182 5.52216 7.95195 4.95688 7.54278 4.5401C7.13361 4.12332 6.57865 3.88918 6 3.88918C5.42135 3.88918 4.86639 4.12332 4.45722 4.5401C4.04805 4.95688 3.81818 5.52216 3.81818 6.11157C3.81818 6.70098 4.04805 7.26626 4.45722 7.68304C4.86639 8.09981 5.42135 8.33396 6 8.33396Z"
-                    fill="#BCC2CC"
-                  />
-                </svg>
-              </div>
-              <SearchAddress>
-                {companyGet.companyAddress
-                  ? companyGet.companyAddress
-                  : companyAddress}
-              </SearchAddress>
-            </AddressBox>
-          </St_Address>
-          <CompanyBtn onClick={togglePopup} value="false">
-            회사 직접 입력
-          </CompanyBtn>
-          {/* <button onClick={togglePopup}>닫기</button> */}
-          {showPopup ? (
+          <RadioBox>
+            <RadioDetail>
+              <input
+                type="radio"
+                id="find"
+                name="companyType"
+                value={"find"}
+                checked={companyHow === "find"}
+                onChange={(e) => {
+                  setCompanyHow(e.target.value);
+                }}
+              />
+              <label htmlFor="find">회사 검색</label>
+            </RadioDetail>
+            <RadioDetail>
+              <input
+                type="radio"
+                id="myself"
+                name="companyHow"
+                value={"myself"}
+                checked={companyHow === "myself"}
+                onChange={(e) => {
+                  setCompanyHow(e.target.value);
+                }}
+              />
+              <label htmlFor="myself">직접 입력</label>
+            </RadioDetail>
+          </RadioBox>
+          {companyHow === "myself" ? (
             <div>
               <St_value
                 placeholder="회사명을 입력하세요"
@@ -431,7 +395,68 @@ const MainCards = () => {
                 <Link to="/posts/companyOtherSearch">회사 주소 검색</Link>
               </AddressSearch>
             </div>
-          ) : null}
+          ) : (
+            <div>
+              <St_value
+                readOnly
+                type="text"
+                name="company"
+                placeholder="회사 검색"
+                value={company || ""}
+                onChange={(e) => {
+                  setCompany(e.target.value);
+                }}
+                onClick={() => {
+                  dispatch(
+                    __cardInfo({
+                      cardName: cardName ? cardName : "",
+                      email: email,
+                      phoneNum: phoneNum,
+                      department: department ? department : "",
+                      position: position ? position : "",
+                      tel: tel,
+                      fax: fax,
+                      companyType: companyType ? companyType : "",
+                    })
+                  );
+                  navigate("/posts/companySearch");
+                }}
+                // readOnly
+              ></St_value>
+              <St_Address
+                name="companyAddress"
+                value={companyAddress || ""}
+                onChange={(e) => {
+                  setCompanyAddress(e.target.value);
+                }}
+              >
+                <AddressBox>
+                  <div>
+                    <svg
+                      width="12"
+                      height="15"
+                      viewBox="0 0 12 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      style={{ marginRight: "8px" }}
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5.66545 14.8907L5.71636 14.9204L5.73673 14.9322C5.81748 14.9767 5.90784 15 5.99964 15C6.09144 15 6.18179 14.9767 6.26255 14.9322L6.28291 14.9211L6.33455 14.8907C6.61899 14.7189 6.89655 14.5356 7.16655 14.3411C7.86551 13.8384 8.51857 13.2726 9.11782 12.6506C10.5316 11.1764 12 8.96141 12 6.11157C12 4.49068 11.3679 2.93618 10.2426 1.79004C9.11742 0.643896 7.5913 0 6 0C4.4087 0 2.88258 0.643896 1.75736 1.79004C0.632141 2.93618 0 4.49068 0 6.11157C0 8.96067 1.46909 11.1764 2.88218 12.6506C3.4812 13.2726 4.13402 13.8384 4.83273 14.3411C5.10296 14.5356 5.38076 14.719 5.66545 14.8907ZM6 8.33396C6.57865 8.33396 7.13361 8.09981 7.54278 7.68304C7.95195 7.26626 8.18182 6.70098 8.18182 6.11157C8.18182 5.52216 7.95195 4.95688 7.54278 4.5401C7.13361 4.12332 6.57865 3.88918 6 3.88918C5.42135 3.88918 4.86639 4.12332 4.45722 4.5401C4.04805 4.95688 3.81818 5.52216 3.81818 6.11157C3.81818 6.70098 4.04805 7.26626 4.45722 7.68304C4.86639 8.09981 5.42135 8.33396 6 8.33396Z"
+                        fill="#BCC2CC"
+                      />
+                    </svg>
+                  </div>
+                  <SearchAddress>
+                    {companyGet.companyAddress
+                      ? companyGet.companyAddress
+                      : companyAddress}
+                  </SearchAddress>
+                </AddressBox>
+              </St_Address>
+            </div>
+          )}
         </Item>
         <Item>
           <St_Key>
@@ -441,7 +466,7 @@ const MainCards = () => {
             type="text"
             placeholder="Ex) 팀장"
             name="position"
-            value={position || ''}
+            value={position || ""}
             onChange={(e) => {
               setPosition(e.target.value);
             }}
@@ -455,7 +480,7 @@ const MainCards = () => {
             type="text"
             placeholder="Ex) 영업"
             name="department"
-            value={department || ''}
+            value={department || ""}
             onChange={(e) => {
               setDepartment(e.target.value);
             }}
@@ -468,12 +493,12 @@ const MainCards = () => {
             placeholder="Ex) 02-000-0000"
             name="tel"
             maxLength="13"
-            value={tel || ''}
+            value={tel || ""}
             onChange={(e) => {
               setTel(e.target.value);
             }}
           ></St_value>
-          {tel && tel.includes('-') === false ? (
+          {tel && tel.includes("-") === false ? (
             <AssistiveText>- 을 포함해주세요</AssistiveText>
           ) : null}
         </Item>
@@ -484,12 +509,12 @@ const MainCards = () => {
             placeholder="Ex) 02-000-0000"
             name="fax"
             maxLength="13"
-            value={fax || ''}
+            value={fax || ""}
             onChange={(e) => {
               setFax(e.target.value);
             }}
           />
-          {fax && fax.includes('-') === false ? (
+          {fax && fax.includes("-") === false ? (
             <AssistiveText>- 을 포함해주세요</AssistiveText>
           ) : null}
         </Item>
