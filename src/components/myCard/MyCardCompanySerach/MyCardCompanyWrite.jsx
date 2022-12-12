a531c0aec0b9e35e9bc044dd90821973c9f3fdc5
andamiro98 <rlatmdwo9@gmail.com>
2022-12-13 00:40:16 +0900

    [회사검색 오류수정]

---
+++ src/components/myCard/MyCardCompanySerach/MyCardCompanyWrite.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import DaumPostcode from 'react-daum-postcode';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { saveInfo } from '../../../redux/modules/mycardSlice';
+import Layout from '../../layout/Layout';
+
+const MyCardCompanyWrite = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleComplete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = '';
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== '') {
+        extraAddress +=
+          extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+    }
+    dispatch(saveInfo({ companyAddress: fullAddress }), navigate(-2));
+  };
+
+  const handleSearch = (data) => {};
+
+  return (
+    <Layout>
+      <DaumPostcode onComplete={handleComplete} onSearch={handleSearch} />
+    </Layout>
+  );
+};
+
+export default MyCardCompanyWrite;
